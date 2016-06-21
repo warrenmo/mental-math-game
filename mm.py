26a08d02d1afc21025af992019f661d3b4ba4ea3
wmowkm <wmowkm@gmail.com>
2016-06-21 14:12:01 -0400

    class implemented
    implemented the game using a simple class; will expand to abstract class later to utilize inheritance for various mental math games

--- mm.py
+++ mm.py
@@ -9,42 +9,52 @@ import sys
 import random
 from timeit import default_timer as timer
 
-def main():
+class MMGame(object):
 
-    tt = 0
+    def __init__(self,amin,amax,bmin,bmax):
+        self.time = 0
 
-    while 1:
+        self.amin = amin
+        self.amax = amax
+        self.bmin = bmin
+        self.bmax = bmax
 
-        a = random.randint(10,99)
-        b = random.randint(10,99)
+    def question(self):
+        while 1:
+            a   = random.randint(self.amin, self.amax)
+            b   = random.randint(self.bmin, self.bmax)
+            out = str(a) + ' x ' + str(b) + '\n'
 
-        out = str(a) + ' x ' + str(b) + '\n'
+            start = timer()
 
-        start = timer()
+            ans = input( out )
 
-        ans = input( out )
+            if int(ans) == (a * b):
+                print('\nCorrect! :D\n')
+            else:
+                cor = str(a * b)
+                print('\nIncorrect :(\nThe correct answer is:', cor, '\n')
+            
+            end = timer()
+            
+            self.time += end - start
 
-        if int(ans) == (a * b):
-            print('\nCorrect! :D\n')
-        else:
-            cor = str(a * b)
-            print('\nIncorrect :(\nThe correct answer is:', cor, '\n')
+            wait = input( 'Continue or quit? (c or q)\n' )
+
+            if wait == 'q':
+                print('Goodbye!\n')
+                break
+            elif wait == 'c':
+                print('Cool! Moving on!\n')
+            else:
+                print('Sorry, I don\'t know what that means.')
+
+        print(tt)
+       
+def main():    
         
-        end = timer()
-        
-        tt += end - start
-        
-        wait = input( 'Continue or quit? (c or q)\n' )
-
-        if wait == 'q':
-            print('Goodbye!\n')
-            break
-        elif wait == 'c':
-            print('Cool! Moving on!\n')
-        else:
-            print('Sorry, I do not know what that means.')
-
-    print(tt)
+    game = MMGame(10,99,10,99)
+    game.question()
 
 if __name__ == "__main__":
     main()
