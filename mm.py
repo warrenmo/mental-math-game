630a3f9d2a2d6c0172c1ba6d5023a1cf806133a2
wmowkm <wmowkm@gmail.com>
2016-06-22 13:31:50 -0400

    removed "Summary" altogether
    removed unnecessary use of class for what is essentially just a list (to feed into tabulate);
    reformatted code to fit more with the Python Style Guide

--- mm.py
+++ mm.py
@@ -10,69 +10,47 @@ import random
 from timeit import default_timer as timer
 from tabulate import tabulate
 
-class Summary(object):
-    
-    def __init__(self):
-        self.number   = 0
-        self.problem  = ''
-        self.proposed = 0
-        self.solution = 0
-        self.result   = ''
-        self.time     = 0
-
-    def assign(self, number, problem, proposed, solution, time):
-        self.number   = number
-        self.problem  = problem
-        self.proposed = proposed
-        self.solution = solution
-        self.time     = time
-
-    def assign_result(self, result):
-        self.result = result
-
-    def output(self):
-        return [self.number, self.problem, self.proposed, self.solution, self.result, self.time]
 
 class MMGame(object):
 
     def __init__(self,amin,amax,bmin,bmax):
-        self.time   = 0
+        self.time = 0
         self.qcount = 1
 
-        self.amin   = amin
-        self.amax   = amax
-        self.bmin   = bmin
-        self.bmax   = bmax
+        self.amin = amin
+        self.amax = amax
+        self.bmin = bmin
+        self.bmax = bmax
 
-        self.rlist  = []
+        self.summary = []
 
     def game(self):
         while True:
-            s   = Summary()
+            l = []
 
-            a   = random.randint(self.amin, self.amax)
-            b   = random.randint(self.bmin, self.bmax)
+            a = random.randint(self.amin, self.amax)
+            b = random.randint(self.bmin, self.bmax)
 
-            number   = '\n(' + str(self.qcount) + ') '
-            problem  = str(a) + ' x ' + str(b)
-            answer   = '\nAnswer: '
-            prompt   = number + problem + answer
+            number = '\n(' + str(self.qcount) + ') '
+            problem = str(a) + ' x ' + str(b)
+            answer = '\nAnswer: '
+            prompt = number + problem + answer
 
             start = timer()
 
-            ans   = input(prompt)
+            ans = input(prompt)
 
-            end   = timer()
+            end = timer()
             self.time += end - start
 
             if ans.isdigit():
                 if int(ans) == (a*b):
                     print('\nCorrect! :D')
-                    s.assign_result('correct')
+                    l.append('correct')
                 else:
                     cor = str(a*b)
                     print('\nIncorrect :(\nThe correct answer is:', cor)
-                    s.assign_result('INCORRECT')
+                    l.append('INCORRECT')
             else:
                 if ans == 'q':
                     wait = input( '\nWould you like to quit? (y or n): ' )
@@ -84,21 +62,21 @@ class MMGame(object):
                     else:
                         print('Sorry, I don\'t know what that means.')
 
-            s.assign(self.qcount, problem, int(ans), a*b, end-start)
-            out = s.output()
-            self.rlist.append(out)
-
+            l += [self.qcount, problem, int(ans), a*b, end-start]
+            self.rlist.append(l)
             self.qcount += 1
 
     def output(self):
         return self.rlist
-       
+
+
 def main():    
     g = MMGame(10,99,10,99)
     g.game()
     
-    table = tabulate(g.output(), headers=['Question Number', 'Problem', 'Your Answer', 'Correct Answer', 'Result', 'Time Taken'])
+    table = tabulate(g.output(), headers=['Result', 'Question Number', 'Problem', 'Your Answer', 'Correct Answer', 'Time Taken'])
     print(table)
+
 
 if __name__ == "__main__":
     main()
